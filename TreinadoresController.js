6a3a9e16de15fcfa9ff9ed3537516f198958e474
Cláudia <claudiamary.santos@gmail.com>
2019-06-24 22:25:20 -0300

    primeiro commit

---
+++ TreinadoresController.js
@@ -0,0 +1,89 @@
+const { connect } = require('./PokemonsApiRepository')
+const treinadoresModel = require('./TreinadoresSchema')
+const { pokemonsModel } = require('./PokemonsSchema')
+const LIMITE_NIVEL_POKEMON = 150
+
+connect()
+
+const calcularNivel = (datas, nivelAnterior) => {
+  const diff = Math.abs(new Date(datas.dataInicio) - new Date(datas.dataFim)) / 3600000
+  const novoNivel = diff / 4 + nivelAnterior;
+
+  return novoNivel >= LIMITE_NIVEL_POKEMON ? LIMITE_NIVEL_POKEMON : novoNivel;
+}
+
+const getAll = () => {
+  return treinadoresModel.find((error, treinadores) => {
+    return treinadores
+  })
+}
+
+const getById = (id) => {
+  return treinadoresModel.findById(id)
+}
+
+const add = (treinador) => {
+  const novoTreinador = new treinadoresModel(treinador)
+  return novoTreinador.save()
+}
+
+const remove = (id) => {
+  return treinadoresModel.findByIdAndDelete(id)
+}
+
+const update = (id, treinador) => {
+  return treinadoresModel.findByIdAndUpdate(
+    id,
+    { $set: treinador },
+    { new: true },
+  )
+}
+
+const addPokemon = async (treinadorId, pokemon) => {
+  const treinador = await getById(treinadorId)
+  const novoPokemon = new pokemonsModel(pokemon)
+
+  treinador.pokemons.push(novoPokemon)
+  return treinador.save()
+}
+
+const treinarPokemon = async (treinadorId, pokemonId, datas) => {
+  const treinador = await getById(treinadorId)
+  const pokemon = treinador.pokemons.find(pokemon => pokemon._id == pokemonId)
+
+  if (pokemon.nivel >= LIMITE_NIVEL_POKEMON) {
+    throw new Error('Seu pokémon já é forte o suficiente!')
+  }
+
+  pokemon.nivel = calcularNivel(datas, pokemon.nivel)
+  return treinador.save()
+}
+
+const getPokemon = async (treinadorId, pokemonId) => {
+  const treinador = await getById(treinadorId)
+
+  const pokemon = treinador.pokemons.find(pokemon => pokemon._id == pokemonId)
+
+  return pokemon;
+}
+
+const updatePokemon = async (treinadorId, pokemonId, pokemon) => {
+  const treinador = await getById(treinadorId)
+  return treinador.pokemons.findByIdAndUpdate(
+    pokemonId,
+    { $set: pokemon },
+    { new: true },
+  )
+}
+
+module.exports = {
+  getAll,
+  getById,
+  add,
+  remove,
+  update,
+  addPokemon,
+  treinarPokemon,
+  getPokemon,
+  updatePokemon
+}
---
+++ server.js
@@ -0,0 +1,163 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const servidor = express()
+const pokemonsController = require('./PokemonsController')
+const treinadoresController = require('./TreinadoresController')
+const params = require('params')
+const parametrosPermitidos = require('./parametrosPermitidos')
+const PORT = 3000
+const logger = (request, response, next) => {
+  console.log(`${new Date().toISOString()} Request type: ${request.method} to ${request.originalUrl}`)
+
+  response.on('finish', () => {
+    console.log(`${response.statusCode} ${response.statusMessage};`)
+  })
+
+  next()
+}
+
+servidor.use(cors())
+servidor.use(bodyParser.json())
+servidor.use(logger)
+
+servidor.get('/', (request, response) => {
+  response.send('Olá, mundo!')
+})
+
+servidor.get('/pokemons', async (request, response) => {
+  pokemonsController.getAll()
+    .then(pokemons => response.send(pokemons))
+})
+
+servidor.get('/treinador/:treinadorId/pokemon/:pokemonId', (request, response) => {
+  const treinadorId = request.params.treinadorId
+  const pokemonId = request.params.pokemonId
+  treinadoresController.getPokemon(treinadorId, pokemonId)
+    .then(pokemon => {
+      if (!pokemon) {
+        response.sendStatus(404)
+      } else {
+        response.send(pokemon)
+      }
+    })
+    .catch(error => {
+      if (error.name === "CastError") {
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.patch('/treinador/:treinadorId/pokemon/:pokemonId', (request, response) => {
+  const treinadorId = request.params.treinadorId
+  const pokemonId = request.params.pokemonId
+  treinadoresController.updatePokemon(treinadorId, pokemonId, request.body)
+    .then(pokemon => {
+      if (!pokemon) { response.sendStatus(404) }
+      else { response.send(pokemon) }
+    })
+    .catch(error => {
+      if (error.name === "MongoError" || error.name === "CastError") {
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+// Rotas TREINADORES
+
+servidor.get('/treinadores', async (request, response) => {
+  treinadoresController.getAll()
+    .then(treinadores => response.send(treinadores))
+})
+
+servidor.get('/treinadores/:treinadorId', (request, response) => {
+  const treinadorId = request.params.treinadorId
+  treinadoresController.getById(treinadorId)
+    .then(treinador => {
+      if (!treinador) {
+        response.sendStatus(404)
+      } else {
+        response.send(treinador)
+      }
+    })
+    .catch(error => {
+      if (error.name === "CastError") {
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.patch('/treinadores/:id', (request, response) => {
+  const id = request.params.id
+  treinadoresController.update(id, request.body)
+    .then(treinador => {
+      if (!treinador) { response.sendStatus(404) }
+      else { response.send(treinador) }
+    })
+    .catch(error => {
+      if (error.name === "MongoError" || error.name === "CastError") {
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.post('/treinadores', (request, response) => {
+  treinadoresController.add(request.body)
+    .then(treinador => {
+      const _id = treinador._id
+      response.send(_id)
+    })
+    .catch(error => {
+      if (error.name === "ValidationError") {
+        response.sendStatus(400)
+      } else {
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.post('/treinadores/adicionar-pokemon/:treinadorId', (request, response) => {
+  const treinadorId = request.params.treinadorId
+  treinadoresController.addPokemon(treinadorId, request.body)
+    .then(treinador => {
+      const _id = treinador._id
+      response.send(_id)
+    })
+    .catch(error => {
+      if (error.name === "ValidationError") {
+        response.sendStatus(400)
+      } else {
+        console.log(error)
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.patch('/treinadores/:treinadorId/treinar/:pokemonId', (request, response) => {
+  const treinadorId = request.params.treinadorId
+  const pokemonId = request.params.pokemonId
+  treinadoresController.treinarPokemon(treinadorId, pokemonId, request.body)
+    .then(treinador => {
+      const _id = treinador._id
+      response.send(_id)
+    })
+    .catch(error => {
+      if (error.name === "ValidationError") {
+        response.sendStatus(400)
+      } else {
+        console.log(error)
+        response.sendStatus(500)
+      }
+    })
+})
+
+servidor.listen(PORT)
+console.info(`Rodando na porta ${PORT}`)
